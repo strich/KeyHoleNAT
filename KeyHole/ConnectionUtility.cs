eaa8ce85e8e49599e791abc6faffb4e6b97ac213
Scott Richmond <s.t.richmond@gmail.com>
2014-01-07 17:29:02 +1100

    Initializing project

---
+++ KeyHole/ConnectionUtility.cs
@@ -0,0 +1,17 @@
+﻿namespace KeyHole {
+    public static class ConnectionUtility {
+
+        public static void CreateServer() { }
+
+        public static void CreateServer(int port, string protocol) { }
+
+        public static void CreateServer(int port, string protocol, string IPAddress) { }
+
+        private static int GetPortViaUpnp()
+        {
+            // TODO - Use UPnP lib to AddPortMapping()
+
+            return 0;
+        }
+    }
+}
